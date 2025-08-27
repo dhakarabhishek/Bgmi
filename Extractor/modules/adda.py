1ddb57a9704707c2ad7c8f01966ad67c43261598
dhakarabhishek <dhakarabhishek07@gmail.com>
2025-08-28 00:22:56 +0530

    Update adda.py

--- Extractor/modules/adda.py
+++ Extractor/modules/adda.py
@@ -327,7 +327,7 @@ async def adda_command_handler(app, m):
                             f"📊 <b>CONTENT STATS</b>\n"
                             f"└─ 📁 Total Items: {total_items}\n\n"
                             f"🚀 <b>Extracted by:</b> {mention}\n\n"
-                            f" <b>For Txt uploading msg here @Final_piece or @chambalchat_bot:</b>\n"
+                            f"<b>For Txt uploading msg here @Final_piece or @chambalchat_bot:</b>\n"
                             f"<code>╾───• {BOT_TEXT} •───╼</code>"
                         )
 
